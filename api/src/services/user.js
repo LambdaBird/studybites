efd921d44ac304dacd05238b71224bb75bc6e9cd
Maksym Herasymov <herasymov.maks@gmail.com>
2021-05-12 15:09:39 +0300

    feat: email and password validation

--- api/src/services/user.js
+++ api/src/services/user.js
@@ -1,3 +1,12 @@
+const yup = require('yup');
+
+const yupOptions = {
+  strict: false,
+  abortEarly: false,
+  stripUnknown: true,
+  recursive: true,
+};
+
 module.exports = async (instance) => {
   instance.route({
     method: 'POST',
@@ -12,6 +21,16 @@ module.exports = async (instance) => {
           secondName: { type: 'string' },
         },
         required: ['email', 'password', 'firstName', 'secondName'],
+        yup: yup.object({
+          email: yup.string().email('"email" must be a valid email string'),
+          password: yup
+            .string()
+            .min(5, '"password" must be at least 5 characters long')
+            .matches(
+              /^(?=.*\d)(?=.*[a-zA-Z]).{5,}$/,
+              '"password" must contain at least one numeric digit and one letter'
+            ),
+        }),
       },
       response: {
         201: {
@@ -20,19 +39,29 @@ module.exports = async (instance) => {
             message: { type: 'string' },
           },
         },
-        400: {
+        '4xx': {
           type: 'object',
           properties: {
             message: { type: 'string' },
           },
         },
-        500: {
+        '5xx': {
           type: 'object',
           properties: {
             message: { type: 'string' },
           },
         },
       },
+    },
+    validatorCompiler: ({ schema }) => {
+      return (data) => {
+        try {
+          return { value: schema.yup.validateSync(data, yupOptions) };
+        } catch (err) {
+          err.message = err.errors.join(', ');
+          return { error: err };
+        }
+      };
     },
     handler: async (req, repl) => {
       return repl.status(201).send({ message: 'success' });
