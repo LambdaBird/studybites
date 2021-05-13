3cce58794c4f0ecdef85e1baa1f1bc3ef6b0a9c6
Maksym Herasymov <herasymov.maks@gmail.com>
2021-05-13 14:56:53 +0300

    remove SyntaxError handler, add helper func for error handlers

--- api/src/validation/errorHandler.js
+++ api/src/validation/errorHandler.js
@@ -1,19 +1,16 @@
+const errorArray = (m) => [].concat(m);
+
 module.exports = (err, repl) => {
   switch (err.name) {
-    case 'SyntaxError':
-      return repl.status(400).send({
-        fallback: 'errors.syntax_error',
-        errors: Array.isArray(err.message) ? err.message : [err.message],
-      });
     case 'ValidationError':
       return repl.status(400).send({
         fallback: 'errors.validation_error',
-        errors: Array.isArray(err.message) ? err.message : [err.message],
+        errors: errorArray(err.message),
       });
     default:
       return repl.status(500).send({
         fallback: 'errors.internal_error',
-        errors: Array.isArray(err.message) ? err.message : [err.message],
+        errors: errorArray(err.message),
       });
   }
 };
