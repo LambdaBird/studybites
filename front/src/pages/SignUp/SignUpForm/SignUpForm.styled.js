f46b2809fd3216a36ecaeb30c3e0a1aa38b3ca77
Vitalik Prac <mastersasori007@gmail.com>
2021-05-14 11:34:20 +0300

    feat: disable import/prefer-default-export rule

--- front/src/pages/SignUp/SignUpForm/SignUpForm.styled.js
+++ front/src/pages/SignUp/SignUpForm/SignUpForm.styled.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { Button } from 'antd';
 
-// eslint-disable-next-line import/prefer-default-export
 export const SubmitButton = styled(Button)`
   width: 100%;
 `;
