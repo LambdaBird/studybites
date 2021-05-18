b0d4a0cc8eadb2e0896186dd1573766594c01c78
Maksym Herasymov <herasymov.maks@gmail.com>
2021-05-18 13:22:40 +0300

    refactor: move authUser to user service

--- api/src/services/user.js
+++ api/src/services/user.js
@@ -131,4 +131,37 @@ const user = async (instance) => {
   });
 };
 
+export const authUser = async (instance, next, req, repl) => {
+  const decoded = await req.jwtVerify();
+  req.userId = decoded.id;
+
+  if (!decoded.access) {
+    return repl.status(401).send({
+      fallback: 'errors.unauthorized',
+      errors: [
+        {
+          message: 'Unauthorized',
+        },
+      ],
+    });
+  }
+
+  const userData = await instance.objection.models.user.query().findOne({
+    id: decoded.id,
+  });
+
+  if (!userData) {
+    return repl.status(401).send({
+      fallback: 'errors.unauthorized',
+      errors: [
+        {
+          message: 'Unauthorized',
+        },
+      ],
+    });
+  }
+
+  return next();
+};
+
 export default user;
