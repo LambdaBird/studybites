1240601a511f34f999a1582e67c51497011e5819
Vitalik Prac <mastersasori007@gmail.com>
2021-05-18 15:10:33 +0300

    refactor: change to test.each in PasswordStrengthIndicator test

--- front/src/components/atoms/PasswordStrengthIndicator/PasswordStrengthIndicator.test.jsx
+++ front/src/components/atoms/PasswordStrengthIndicator/PasswordStrengthIndicator.test.jsx
@@ -3,96 +3,22 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 import PasswordStrengthIndicator from './PasswordStrengthIndicator';
 
 describe('Test PasswordStrengthIndicator', () => {
-  let renderer;
-  beforeEach(() => {
-    renderer = new ShallowRenderer();
-  });
-
-  test('value with no symbols', () => {
-    renderer.render(<PasswordStrengthIndicator value="" />);
+  test.each([
+    ['no symbols', '', -1],
+    ['3 letters', 'abc', 0],
+    ['3 numbers', '123', 0],
+    ['1 upper letter and 3 numbers', 'A123', 0],
+    ['1 upper letter and 1 lower', 'Aa', 0],
+    ['1 upper letter, 1 lower and 3 numbers', 'Aa123', 0],
+    ['1 upper letter, 1 lower, 1 symbol and 3 numbers', 'Aa#123', 1],
+    ['1 upper letter and 5 lower', 'Abcdef', 1],
+    ['1 upper letter, 5 lower, 1 symbol, 1 number', 'Abcdef#1', 2],
+    ['1 upper letter, 5 lower, 1 symbol, 3 number, len >= 10', 'Abcdef#123', 3],
+  ])('value with %s', (_, payload, expected) => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<PasswordStrengthIndicator value={payload} />);
     const result = renderer.getRenderOutput();
     const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(-1);
-  });
-
-  test('value with 3 letters', () => {
-    renderer.render(<PasswordStrengthIndicator value="abc" />);
-    const result = renderer.getRenderOutput();
-    const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(0);
-  });
-
-  test('value with 3 numbers', () => {
-    renderer.render(<PasswordStrengthIndicator value="123" />);
-    const result = renderer.getRenderOutput();
-    const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(0);
-  });
-
-  test('value with 1 upper letter and 3 numbers', () => {
-    renderer.render(<PasswordStrengthIndicator value="A123" />);
-    const result = renderer.getRenderOutput();
-    const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(0);
-  });
-
-  test('value with 1 upper letter and 1 lower', () => {
-    renderer.render(<PasswordStrengthIndicator value="Aa" />);
-    const result = renderer.getRenderOutput();
-    const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(0);
-  });
-
-  test('value with 1 upper letter, 1 lower and 3 numbers', () => {
-    renderer.render(<PasswordStrengthIndicator value="Aa123" />);
-    const result = renderer.getRenderOutput();
-    const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(0);
-  });
-
-  test('value with 1 upper letter, 1 lower, 1 symbol and 3 numbers', () => {
-    renderer.render(<PasswordStrengthIndicator value="Aa#123" />);
-    const result = renderer.getRenderOutput();
-    const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(1);
-  });
-
-  test('value with 1 upper letter and 5 lower', () => {
-    renderer.render(<PasswordStrengthIndicator value="Abcdef" />);
-    const result = renderer.getRenderOutput();
-    const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(1);
-  });
-
-  test('value with 1 upper letter and 5 lower', () => {
-    renderer.render(<PasswordStrengthIndicator value="Abcdef" />);
-    const result = renderer.getRenderOutput();
-    const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(1);
-  });
-
-  test('value with 1 upper letter, 5 lower, 1 symbol, 1 number', () => {
-    renderer.render(<PasswordStrengthIndicator value="Abcdef#1" />);
-    const result = renderer.getRenderOutput();
-    const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(2);
-  });
-
-  test('value with 1 upper letter, 5 lower, 1 symbol, 3 number, len >= 10', () => {
-    renderer.render(<PasswordStrengthIndicator value="Abcdef#123" />);
-    const result = renderer.getRenderOutput();
-    const indicatorDiv = result.props.children;
-
-    expect(indicatorDiv.props.level).toBe(3);
+    expect(indicatorDiv.props.level).toBe(expected);
   });
 });
