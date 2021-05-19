d56c85ee064501c02addfb6d374de342bca82ecd
Vitalik Prac <mastersasori007@gmail.com>
2021-05-19 16:20:42 +0300

    feat: add useSignUp (server responses with unknown key), loading test

--- front/src/hooks/useSignUp/useSignUp.test.js
+++ front/src/hooks/useSignUp/useSignUp.test.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { postSignUp } from '../../utils/api/v1/user';
 import useSignUp from './useSignUp';
 
 jest.mock('../../utils/api/v1/user');
 
-jest.mock('react-i18next', () => ({
-  useTranslation: () => ({
-    t: (x) => x,
-  }),
-}));
+jest.mock('react-i18next');
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
@@ -31,7 +28,41 @@ describe('Test useSignUp', () => {
       mockSetStates = [];
     });
 
+    test('server responses with unknown key', async () => {
+      useTranslation.mockImplementation(() => ({
+        t: (x) => x,
+      }));
+      const [auth] = await useSignUp();
+      postSignUp.mockImplementation(() => ({
+        status: 409,
+        data: {
+          fallback: 'errors.unique_violation',
+          errors: [
+            {
+              key: 'unknown_key',
+              message: 'DEFAULT MESSAGE',
+            },
+          ],
+        },
+      }));
+      await auth({
+        firstName: 'test',
+        lastName: 'test',
+        password: 'password',
+        email: 'hello@gmail.com',
+      });
+
+      expect(mockSetStates[0]).toHaveBeenNthCalledWith(1, null);
+      expect(mockSetStates[0]).toHaveBeenNthCalledWith(2, 'DEFAULT MESSAGE');
+      expect(mockSetStates[1]).toHaveBeenNthCalledWith(1, true);
+      expect(mockSetStates[1]).toHaveBeenNthCalledWith(2, false);
+      expect(mockSetStates[2]).toBeCalledWith(null);
+    });
+
     test('value with already user data', async () => {
+      useTranslation.mockImplementation(() => ({
+        t: () => 'already_registered',
+      }));
       const [auth] = await useSignUp();
       postSignUp.mockImplementation(() => ({
         status: 409,
@@ -53,15 +84,16 @@ describe('Test useSignUp', () => {
       });
 
       expect(mockSetStates[0]).toHaveBeenNthCalledWith(1, null);
-      expect(mockSetStates[0]).toHaveBeenNthCalledWith(
-        2,
-        'This email was already registered',
-      );
-      expect(mockSetStates[1]).toBeCalledWith(true);
+      expect(mockSetStates[0]).toHaveBeenNthCalledWith(2, 'already_registered');
+      expect(mockSetStates[1]).toHaveBeenNthCalledWith(1, true);
+      expect(mockSetStates[1]).toHaveBeenNthCalledWith(2, false);
       expect(mockSetStates[2]).toBeCalledWith(null);
     });
 
     test('value with new user data', async () => {
+      useTranslation.mockImplementation(() => ({
+        t: () => 'action_success',
+      }));
       const [auth] = await useSignUp();
       postSignUp.mockImplementation(() => ({
         status: 201,
@@ -77,12 +109,10 @@ describe('Test useSignUp', () => {
         email: 'hello@gmail.com',
       });
       expect(mockSetStates[0]).toBeCalledWith(null);
-      expect(mockSetStates[1]).toBeCalledWith(true);
+      expect(mockSetStates[1]).toHaveBeenNthCalledWith(1, true);
+      expect(mockSetStates[1]).toHaveBeenNthCalledWith(2, false);
       expect(mockSetStates[2]).toHaveBeenNthCalledWith(1, null);
-      expect(mockSetStates[2]).toHaveBeenNthCalledWith(
-        2,
-        'Successfully signed up',
-      );
+      expect(mockSetStates[2]).toHaveBeenNthCalledWith(2, 'action_success');
     });
   });
 });
