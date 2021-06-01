000a0e7c4b46a5673344b62751c1c06121e006f1
ptatarsk <ptatarsk@gmail.com>
2021-06-01 16:00:41 +0300

    feat: dispaly students if exist in students list

--- front/src/pages/TeacherHome/StudentsList/StudentsList.jsx
+++ front/src/pages/TeacherHome/StudentsList/StudentsList.jsx
@@ -1,41 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import { Button, Typography } from 'antd';
+import { Button, Typography, Divider, Row, Col, Space, Avatar } from 'antd';
 import * as S from './StudentsList.styled';
-import emptyImage from '../../../resources/img/empty_students.svg'
+import emptyImage from '../../../resources/img/empty_students.svg';
 
 const { Text } = Typography;
 
-const StudentsList = ({ students }) => {
+const StudentsList = ({ students, loading }) => {
   const { t } = useTranslation();
 
   return (
     <S.Wrapper>
-      <S.ListTitle level={4}>{t('students_list.title')}</S.ListTitle>
-      {(students && !students.length) 
-        ? <S.EmptyList
+      {loading || !students?.length ? (
+        <>
+          <S.EmptyListHeader>
+            <S.ListTitle level={4}>{t('students_list.title')}</S.ListTitle>
+          </S.EmptyListHeader>
+          <S.EmptyList
             image={emptyImage}
             imageStyle={{
               height: 60,
             }}
-            description={
-              <Text>{t('students_list.no_students')}</Text>
-            }
+            description={<Text>{t('students_list.no_students')}</Text>}
           >
-          <Button type="link">{t('students_list.invite')}</Button>
+            <Button type="link" onClick={() => {}}>
+              {t('students_list.invite')}
+            </Button>
           </S.EmptyList>
-      : null}
+        </>
+      ) : (
+        <>
+          <S.ListHeader>
+            <S.ListTitle level={4}>{t('students_list.title')}</S.ListTitle>
+            <Button type="link" onClick={() => {}}>
+              {t('students_list.all')}
+            </Button>
+          </S.ListHeader>
+          <Divider />
+          <Row justify="center" align="top">
+            {students.map((el) => (
+              <Col span={12}>
+                <Space>
+                  <Avatar src={el.cover} />
+                  <Text>{el.name}</Text>
+                </Space>
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </S.Wrapper>
   );
 };
 
 StudentsList.propTypes = {
-  students: PropTypes.arrayOf(PropTypes.shape({
-    cover: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    id: PropTypes.number.isRequired,
-  })),
+  students: PropTypes.arrayOf(
+    PropTypes.shape({
+      cover: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+  loading: PropTypes.bool.isRequired,
 };
 
 StudentsList.defaultProps = {
--- front/src/pages/TeacherHome/StudentsList/StudentsList.styled.js
+++ front/src/pages/TeacherHome/StudentsList/StudentsList.styled.js
@@ -10,14 +10,28 @@ export const Wrapper = styled(Row)`
   justify-content: flex-start;
   align-items: center;
   padding: 16px 24px 16px 24px;
-	box-shadow: 0px 4px 4px 0px rgba(240, 241, 242, 1);
+  box-shadow: 0px 4px 4px 0px rgba(240, 241, 242, 1);
   background: rgba(255, 255, 255, 1);
 `;
 
 export const ListTitle = styled(Title)`
-  align-self: flex-start;
+  padding-top: 0.5rem;
 `;
 
 export const EmptyList = styled(Empty)`
   padding-top: 64px;
+`;
+
+export const EmptyListHeader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const ListHeader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
