f050bac9b8598c294f44bf7ab9c4cb0b15eca3da
ptatarsk <ptatarsk@gmail.com>
2021-06-01 15:47:51 +0300

    fix: useGetStudentsHook lint

--- front/src/hooks/useGetStudents/useGetStudents.js
+++ front/src/hooks/useGetStudents/useGetStudents.js
@@ -5,24 +5,21 @@ const useGetStudents = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [studentsData, setStudentsData] = useState(null);
 
-  const getStudentsRequest = useCallback(
-    async () => {
-      setIsLoading(true);
-  
-      const { status, data } = await getStudents();
-      setIsLoading(false);
-  
-      if (status === 200) {
-        setStudentsData(data);
-      }
-    },
-    [],
-  )
+  const getStudentsRequest = useCallback(async () => {
+    setIsLoading(true);
+
+    const { status, data } = await getStudents();
+    setIsLoading(false);
+
+    if (status === 200) {
+      setStudentsData(data);
+    }
+  }, []);
 
   return {
     getStudentsRequest,
     studentsData,
-    isLoading
+    isLoading,
   };
 };
 
