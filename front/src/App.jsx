ba77734c34a034b79f1b265d8025c90cf2da8f4e
ptatarsk <ptatarsk@gmail.com>
2021-06-01 15:42:07 +0300

    feat: show teacher home page component in App

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
-import TeacherHome from './pages/TeacherHome'
+import TeacherHome from './pages/TeacherHome';
 
 const App = () => (
   <Router>
