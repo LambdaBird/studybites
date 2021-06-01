946f7abe69d8c4b50885c30c5441253a76e45fa3
ptatarsk <ptatarsk@gmail.com>
2021-06-01 15:58:54 +0300

    feat: add studenst list in card, lint

--- front/src/pages/TeacherHome/LessonsDashboard/LessonCard/LessonCard.jsx
+++ front/src/pages/TeacherHome/LessonsDashboard/LessonCard/LessonCard.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import { Typography } from 'antd';
+import { Typography, Space, Avatar, Tooltip } from 'antd';
 import * as S from './LessonCard.styled';
 
 const { Title, Text } = Typography;
 
 const LessonCard = ({ cover, title, students }) => {
   const { t } = useTranslation();
-    
+
   return (
     <S.Wrapper justify="center" align="center">
-      <S.ImageCol span={8} >
-        <S.CardImage
-          src={cover}
-        />
+      <S.ImageCol span={8}>
+        <S.CardImage src={cover} />
       </S.ImageCol>
-      <S.CardDescription span={16} >
+      <S.CardDescription span={16}>
         <S.CardText>
           <Title level={4}>{title}</Title>
-          {!students.length
-            ? <Text type="secondary">{t('lesson_dashboard.card.no_students')}</Text>
-            : null
-          }
+          {!students.length ? (
+            <Text type="secondary">
+              {t('lesson_dashboard.card.no_students')}
+            </Text>
+          ) : (
+            <Space>
+              <Avatar.Group maxCount={3}>
+                {students.map((el) => (
+                  <Tooltip key={el.id} title={el.name} placement="top">
+                    <Avatar src={el.avatar} />
+                  </Tooltip>
+                ))}
+              </Avatar.Group>
+              <Text type="secondary">
+                {students.length} {t('lesson_dashboard.card.students')}
+              </Text>
+            </Space>
+          )}
         </S.CardText>
         <S.CardButton>{t('lesson_dashboard.card.edit')}</S.CardButton>
       </S.CardDescription>
@@ -33,9 +45,12 @@ const LessonCard = ({ cover, title, students }) => {
 LessonCard.propTypes = {
   cover: PropTypes.string,
   title: PropTypes.string,
-  students: PropTypes.arrayOf(PropTypes.shape({
-    avatar: PropTypes.string.isRequired,
-  })),
+  students: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ),
 };
 
 LessonCard.defaultProps = {
