07cb75015ddb7e82b5e21e1e72dca4d3c3e51f7b
ptatarsk <ptatarsk@gmail.com>
2021-06-02 13:19:26 +0300

    feat: add user with lessons script

---
+++ api/utils/addUserWithsLessons.js
@@ -0,0 +1,88 @@
+import bcrypt from 'bcrypt';
+import config from '../config.json';
+import build from '../src/app.js';
+
+const app = await build();
+
+const addUser = async ({ id, email, password, firstName, secondName }) => {
+  const data = await app.objection.models.user.transaction(async (trx) => {
+    const passwd = await bcrypt.hash(password, 12);
+    const user = await app.objection.models.user
+    .query(trx)
+    .insert({
+      id,
+      email,
+      password: passwd,
+      firstName,
+      secondName,
+    })
+    .returning('*');
+    await app.objection.models.userRole
+      .query(trx)
+      .insert({
+        userID: user.id,
+        roleID: config.roles.MAINTAINER_ROLE,
+        resourceType: config.resources.LESSON,
+      })
+      .returning('*');
+    return user;
+  });
+  return data;
+}; 
+
+const addLessons = async ({ name, description, status, userId }) => {
+  const data = await app.objection.models.lesson.transaction(async (trx) => {
+    const lesson = await app.objection.models.lesson
+      .query(trx)
+      .insert({
+        name,
+        description,
+        status,
+      })
+      .returning('*');
+    
+    await app.objection.models.userRole
+      .query(trx)
+      .insert({
+        userID: userId,
+        roleID: config.roles.MAINTAINER_ROLE,
+        resourceType: 'lesson',
+        resourceId: lesson.id,
+      })
+      .returning('*');
+    return lesson;
+  });
+  return data;
+};
+
+await addUser({
+  id: 6,
+  email: 'test111@gmail.com',
+  password: 'test111',
+  firstName: 'Test',
+  secondName: 'Teacher',
+});
+
+await addLessons({
+  name: 'How to use Studybites',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dignissimos eaque, hic magnam nemo officiis porro quidem recusandae repellat repellendus.',
+  status: 'Public',
+  userId: 6,
+});
+
+await addLessons({
+  name: 'Test lesson',
+  description:
+    'Adipisci dignissimos eaque, hic magnam nemo officiis porro quidem recusandae repellat repellendus.',
+  status: 'Draft',
+  userId: 6,
+});
+
+await addLessons({
+  name: 'New test',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+  status: 'Archived',
+  userId: 6,
+});
