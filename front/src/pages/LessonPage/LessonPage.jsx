4a4160b11dd732514ab6987562cd8431d128598c
ptatarsk <ptatarsk@gmail.com>
2021-06-03 19:17:26 +0300

    feat: add progress, button bar and update styles

--- front/src/pages/LessonPage/LessonPage.jsx
+++ front/src/pages/LessonPage/LessonPage.jsx
@@ -1,25 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Col } from 'antd';
+import { Progress } from 'antd';
+import { useTranslation } from 'react-i18next';
 import useGetLessonToLearn from '../../hooks/useGetLessonToLearn';
 import InfoBlock from './InfoBlock';
 import * as S from './LessonPage.styled';
 
 const LessonPage = () => {
+  const [percents, setPercents] = useState(0);
   const { id: lessonId } = useParams();
   const { getLessonToLearnRequest, isError, isLoading, lessonData } = useGetLessonToLearn(lessonId);
+  const { t } = useTranslation();
 
   useEffect(() => {
     getLessonToLearnRequest();
   }, []);
 
-  console.log(isError, isLoading, lessonData);
+  useEffect(() => {
+    if (lessonData && ! isError) {
+      const { data: { blocks }, data: { progress } } = lessonData;
+      const hundredPercents = 100;
+      const progressPercents = progress * hundredPercents / blocks;
+      setPercents(progressPercents.toFixed(2));
+    }
+  }, [lessonData]);
 
   return (
-    <S.Page justify="center" align="top">
-      <Col xs={{ span: 20 }} sm={{ span: 18 }} md={{ span: 16 }} lg={{ span: 12 }} >
-        <InfoBlock isLoading={isLoading} isError={isError} lessonData={lessonData} />
-      </Col>
+    <S.Page>
+      <S.ProgressCol span={24}>
+        <Progress showInfo={false} percent={percents} strokeWidth={4} strokeLinecap='round' />
+      </S.ProgressCol>
+      <S.PageRow justify="center" align="top">
+        <S.BlockCol xs={{ span: 20 }} sm={{ span: 18 }} md={{ span: 16 }} lg={{ span: 12 }}>
+          <InfoBlock isLoading={isLoading} isError={isError} lessonData={lessonData} />
+        </S.BlockCol>
+      </S.PageRow>
+      <S.PageRow justify="center" align="top">
+        <S.BlockCol xs={{ span: 22 }} sm={{ span: 20 }} md={{ span: 18 }} lg={{ span: 12 }}>
+          <S.LessonButton>{t('lesson.start')}</S.LessonButton>
+        </S.BlockCol>
+      </S.PageRow>
     </S.Page>
   );
 };
--- front/src/pages/LessonPage/LessonPage.styled.js
+++ front/src/pages/LessonPage/LessonPage.styled.js
@@ -1,8 +1,34 @@
 import styled from 'styled-components';
-import { Row } from 'antd';
+import { Row, Col, Button } from 'antd';
 
-export const Page = styled(Row)`
+export const Page = styled.div`
   width: 100%;
-  height: 100%;
+  height: 92vh;
   background-color: rgba(255, 255, 255, 1);
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 576px) {
+    justify-content: space-between;
+  }
+`;
+
+export const PageRow = styled(Row)`
+  width: 100%;
+`;
+
+export const BlockCol = styled(Col)`
+  display: flex;
+  justify-content: center;
+  padding-top: 3rem;
+`;
+
+export const ProgressCol = styled(Col)`
+  width: 100%;
+  position: absolute;
+  top: 41px;
+`;
+
+export const LessonButton = styled(Button)`
+  width: 100%;
+  max-width: 614px;
 `;
