e4b5ac30e3a258e18be93d35074b3b3babb7b33b
Maksym Herasymov <herasymov.maks@gmail.com>
2021-06-15 11:05:26 +0300

    feature: lesson creation with blocks

--- api/src/services/lesson/routes.js
+++ api/src/services/lesson/routes.js
@@ -1,3 +1,5 @@
+import { v4 } from 'uuid';
+
 import config from '../../../config';
 
 import errorResponse from '../../validation/schemas';
@@ -13,7 +15,7 @@ import {
 import { NOT_FOUND, INVALID_ENROLL, ENROLL_SUCCESS } from './constants';
 
 const router = async (instance) => {
-  const { Lesson, UserRole } = instance.models;
+  const { Lesson, UserRole, Block, LessonBlockStructure } = instance.models;
 
   instance.route({
     method: 'GET',
@@ -192,6 +194,30 @@ const router = async (instance) => {
               resourceId: lesson.id,
             })
             .returning('*');
+
+          if (req.body.blocks) {
+            const blocksData = await Block.query(trx)
+              .insert(req.body.blocks)
+              .returning('blockId');
+
+            const lessonBlock = [];
+
+            for (let i = 0, n = blocksData.length; i < n; i += 1) {
+              lessonBlock.push({
+                id: v4(),
+                lessonId: lesson.id,
+                blockId: blocksData[i].blockId,
+              });
+            }
+
+            for (let i = 0, n = lessonBlock.length; i < n; i += 1) {
+              lessonBlock[i].parentId = !i ? null : lessonBlock[i - 1].id;
+              lessonBlock[i].childId =
+                i === n - 1 ? null : lessonBlock[i + 1].id;
+            }
+
+            await LessonBlockStructure.query(trx).insert(lessonBlock);
+          }
 
           return lesson;
         });
--- api/src/services/lesson/validators.js
+++ api/src/services/lesson/validators.js
@@ -20,6 +20,30 @@ const statusValidator = yup
   .typeError(propertyTypeError('lesson', 'status', 'string'))
   .oneOf(['Draft', 'Public', 'Private', 'Archived'], INVALID_STATUS);
 
+const blocksValidator = yup
+  .array()
+  .typeError(propertyTypeError('lesson', 'blocks', 'array'))
+  .of(
+    yup.object().shape({
+      revisionId: yup
+        .string()
+        .typeError(propertyTypeError('lesson', 'revisionId', 'string'))
+        .required(requiredPropertyError('lesson', 'revisionId')),
+      content: yup
+        .object()
+        .typeError(propertyTypeError('lesson', 'content', 'object')),
+      type: yup
+        .string()
+        .typeError(propertyTypeError('lesson', 'type', 'string')),
+      answer: yup
+        .object()
+        .typeError(propertyTypeError('lesson', 'answer', 'object')),
+      weight: yup
+        .number()
+        .typeError(propertyTypeError('lesson', 'weight', 'number')),
+    }),
+  );
+
 const nameValidatorPatch = yup
   .string()
   .typeError(propertyTypeError('lesson', 'name', 'string'));
@@ -28,6 +52,7 @@ export const postBodyValidator = yup.object({
   name: nameValidatorPost,
   description: descriptionValidator,
   status: statusValidator,
+  blocks: blocksValidator,
 });
 
 export const patchBodyValidator = yup.object({
