5d2bd2c8daae695afc8c48bcde75df72204e59ce
Maksym Herasymov <herasymov.maks@gmail.com>
2021-06-15 21:49:28 +0300

    feature: lesson update with blocks

--- api/src/services/lesson/routes.js
+++ api/src/services/lesson/routes.js
@@ -1,4 +1,5 @@
 import { v4 } from 'uuid';
+import objection from 'objection';
 
 import config from '../../../config';
 
@@ -8,7 +9,7 @@ import errorHandler from '../../validation/errorHandler';
 import { BadRequestError, NotFoundError } from '../../validation/errors';
 
 import {
-  patchBodyValidator,
+  // patchBodyValidator,
   postBodyValidator,
   validateId,
 } from './validators';
@@ -229,10 +230,10 @@ const router = async (instance) => {
   });
 
   instance.route({
-    method: 'PATCH',
+    method: 'PATCH', // put
     url: '/maintain/:id',
     schema: {
-      body: patchBodyValidator,
+      // body: patchBodyValidator,
       response: errorResponse,
     },
     validatorCompiler,
@@ -245,24 +246,86 @@ const router = async (instance) => {
       getId: (req) => req.params.id,
     }),
     handler: async (req, repl) => {
-      const id = validateId(req.params.id);
+      try {
+        await Lesson.transaction(async (trx) => {
+          const id = validateId(req.params.id);
 
-      const data = await UserRole.relatedQuery('lessons')
-        .for(
-          UserRole.query().select().where({
-            userID: req.user.id,
-            roleID: config.roles.MAINTAINER.id,
-            resourceId: id,
-          }),
-        )
-        .patch(req.body)
-        .returning('*');
+          const { lesson, blocks } = req.body;
 
-      if (!data) {
-        throw new NotFoundError(NOT_FOUND);
+          if (lesson) {
+            await UserRole.relatedQuery('lessons')
+              .for(
+                UserRole.query().select().where({
+                  userID: req.user.id,
+                  roleID: config.roles.MAINTAINER.id,
+                  resourceId: id,
+                }),
+              )
+              .patch(req.body)
+              .returning('*');
+          }
+
+          if (blocks) {
+            const revisions = await Block.query(trx)
+              .select(
+                objection.raw(
+                  `json_object_agg(x.block_id, x.revisions) as values`,
+                ),
+              )
+              .from(
+                objection.raw(
+                  `(select block_id, array_agg(revision_id) as revisions from blocks group by block_id) as x`,
+                ),
+              );
+
+            const { values } = revisions[0];
+
+            const blocksToInsert = [];
+
+            for (let i = 0, n = blocks.length; i < n; i += 1) {
+              const { revisionId, blockId } = blocks[i];
+
+              if (revisionId && !blockId) {
+                blocks[i].blockId = v4();
+                blocksToInsert.push(blocks[i]);
+              }
+
+              if (revisionId && blockId) {
+                if (values[blockId] && !values[blockId].includes(revisionId)) {
+                  blocksToInsert.push(blocks[i]);
+                }
+              }
+            }
+
+            await Block.query(trx).insert(blocksToInsert).returning('*');
+
+            const blockStructure = [];
+
+            for (let i = 0, n = blocks.length; i < n; i += 1) {
+              blockStructure.push({
+                id: v4(),
+                lessonId: id,
+                blockId: blocks[i].blockId,
+              });
+            }
+
+            for (let i = 0, n = blockStructure.length; i < n; i += 1) {
+              blockStructure[i].parentId = !i ? null : blockStructure[i - 1].id;
+              blockStructure[i].childId =
+                i === n - 1 ? null : blockStructure[i + 1].id;
+            }
+
+            await LessonBlockStructure.query(trx)
+              .delete()
+              .where({ lessonId: id });
+            await LessonBlockStructure.query(trx).insert(blockStructure);
+          }
+        });
+
+        return repl.status(200).send({ status: 'ok' });
+      } catch (err) {
+        throw new Error(err);
       }
-
-      return repl.status(200).send({ data });
     },
   });
 
