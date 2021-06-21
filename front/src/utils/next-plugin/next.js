0876ee8c63849e0f9225f2d8b8623b77009b77f7
Vitaliy Prachov <mastersasori007@gmail.com>
2021-06-21 14:29:33 +0300

    chore: change Next icon

--- front/src/utils/next-plugin/next.js
+++ front/src/utils/next-plugin/next.js
@@ -6,7 +6,8 @@ export default class Next {
   static get toolbox() {
     return {
       title: 'Next',
-      icon: `<svg width="19" height="4" viewBox="0 0 19 4" xmlns="http://www.w3.org/2000/svg"><path d="M1.25 0H7a1.25 1.25 0 1 1 0 2.5H1.25a1.25 1.25 0 1 1 0-2.5zM11 0h5.75a1.25 1.25 0 0 1 0 2.5H11A1.25 1.25 0 0 1 11 0z"/></svg>`,
+      icon: `<svg height="14" width="12"><text font-size='1.5em' x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">N</text></svg>
+ `,
     };
   }
 
