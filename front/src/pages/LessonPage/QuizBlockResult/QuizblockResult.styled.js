3d2e6428d856de269ecfacd0f70ad0bc683b982a
Vitaliy Prachov <mastersasori007@gmail.com>
2021-06-28 16:53:51 +0300

    feat: make styles quiz block correct

--- front/src/pages/LessonPage/QuizBlockResult/QuizblockResult.styled.js
+++ front/src/pages/LessonPage/QuizBlockResult/QuizblockResult.styled.js
@@ -1,14 +1,6 @@
 import styled from 'styled-components';
-import { Checkbox, Row } from 'antd';
+import { Row } from 'antd';
 
 export const StyledRow = styled(Row)`
   width: 100%;
-`;
-
-export const ColumnCheckbox = styled(Checkbox.Group)`
-  display: flex;
-  flex-direction: column;
-  input[type='checkbox'] {
-    background-color: green;
-  }
 `;
