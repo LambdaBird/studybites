900ea0cb3cf19622d726baa62f12c8c62ba808e3
Maksym Herasymov <herasymov.maks@gmail.com>
2021-07-19 09:52:06 +0300

    feature: add checkAllowed to check for allowed actions in learning flow

--- api/src/services/lesson/handlers/learnLesson.js
+++ api/src/services/lesson/handlers/learnLesson.js
@@ -82,6 +82,84 @@ export const options = {
   },
 };
 
+export async function checkAllowed({
+  userId,
+  lessonId,
+  Result,
+  LessonBlockStructure,
+}) {
+  /**
+   * variable for the last block of a chunk
+   */
+  let block;
+  /**
+   * get a last record from the results table
+   */
+  const lastResult = await Result.getLastResult({ userId, lessonId });
+  /**
+   * if no records in the results table return 'start' as the allowed action
+   */
+  if (!lastResult) {
+    return {
+      allowed: { action: 'start' },
+    };
+  }
+  /**
+   * if last record is the 'finish' action return null as allowed
+   * to throw error inside the route handler
+   */
+  if (lastResult.action === 'finish') {
+    return {
+      allowed: null,
+    };
+  }
+  /**
+   * if last record is the 'start' action get a chunk of blocks from start
+   * to the next interactive block and return allowed action based on blocks type
+   */
+  if (lastResult.action === 'start') {
+    const chunk = await LessonBlockStructure.getChunk({ lessonId });
+    block = chunk[chunk.length - 1];
+  }
+  /**
+   * else get a chunk of blocks from last record to the next interactive
+   * and return allowed action based on blocks type
+   */
+  if (!block) {
+    const chunk = await LessonBlockStructure.getChunk({
+      lessonId,
+      previousBlock: lastResult.blockId,
+    });
+    block = chunk[chunk.length - 1];
+  }
+
+  /**
+   * if block is undefined -> finish
+   */
+  if (!block) {
+    return {
+      allowed: { action: 'finish' },
+    };
+  }
+  /**
+   * find allowed action based on blocks type
+   */
+  switch (block.type) {
+    case 'next':
+      return {
+        allowed: { action: 'next' },
+      };
+    case 'quiz':
+      return {
+        allowed: { action: 'response' },
+      };
+    default:
+      return {
+        allowed: { action: 'finish' },
+      };
+  }
+}
+
 export async function handler({
   user: { id: userId },
   params: { lessonId },
