0e458fc2f3759d473a773c168e2b0d27a1400320
Maksym Herasymov <herasymov.maks@gmail.com>
2021-07-28 15:39:12 +0300

    fix: fix API messages usage on front-end

--- api/src/config/lessonService.js
+++ api/src/config/lessonService.js
@@ -1,9 +1,9 @@
 export const lessonServiceErrors = {
-  LESSON_ERR_LESSON_NOT_FOUND: 'lesson.error.invalid_lesson_id',
-  LESSON_ERR_FAIL_ENROLL: 'lesson.error.fail_enroll',
-  LESSON_ERR_FAIL_LEARN: 'lesson.error.fail_learn',
+  LESSON_ERR_LESSON_NOT_FOUND: 'errors.invalid_lesson_id',
+  LESSON_ERR_FAIL_ENROLL: 'errors.fail_enroll',
+  LESSON_ERR_FAIL_LEARN: 'errors.fail_learn',
 };
 
 export const lessonServiceMessages = {
-  LESSON_MSG_SUCCESS_ENROLL: 'lesson.message.success_enroll',
+  LESSON_MSG_SUCCESS_ENROLL: 'messages.success_enroll',
 };
--- api/src/config/userService.js
+++ api/src/config/userService.js
@@ -1,19 +1,19 @@
 export const userServiceErrors = {
-  USER_ERR_UNAUTHORIZED: 'user.error.unauthorized',
-  USER_ERR_TOKEN_EXPIRED: 'user.error.token_expired',
-  USER_ERR_INVALID_USER_ID: 'user.error.invalid_user_id',
-  USER_ERR_ALREADY_REGISTERED: 'user.error.user_already_registered',
-  USER_ERR_USER_NOT_FOUND: 'user.error.user_not_found',
-  USER_ERR_INVALID_UPDATE: 'user.error.invalid_update',
-  USER_ERR_ROLE_NOT_FOUND: 'user.error.role_not_found',
-  USER_ERR_FAIL_ALTER_ROLE: 'user.error.fail_alter_role',
-  USER_ERR_MISSING_ROLE: 'user.error.missing_role',
+  USER_ERR_UNAUTHORIZED: 'errors.unauthorized',
+  USER_ERR_TOKEN_EXPIRED: 'errors.token_expired',
+  USER_ERR_INVALID_USER_ID: 'errors.invalid_user_id',
+  USER_ERR_ALREADY_REGISTERED: 'errors.user_already_registered',
+  USER_ERR_USER_NOT_FOUND: 'errors.user_not_found',
+  USER_ERR_INVALID_UPDATE: 'errors.invalid_update',
+  USER_ERR_ROLE_NOT_FOUND: 'errors.role_not_found',
+  USER_ERR_FAIL_ALTER_ROLE: 'errors.fail_alter_role',
+  USER_ERR_MISSING_ROLE: 'errors.missing_role',
 };
 
 export const userServiceMessages = {
-  USER_MSG_USER_DELETED: 'user.message.user_deleted',
-  USER_MSG_SUCCESS_ALTER_ROLE: 'user.message.success_alter_role',
-  USER_MSG_ROLE_DELETED: 'user.message.role_deleted',
+  USER_MSG_USER_DELETED: 'messages.user_deleted',
+  USER_MSG_SUCCESS_ALTER_ROLE: 'messages.success_alter_role',
+  USER_MSG_ROLE_DELETED: 'messages.role_deleted',
 };
 
 export const userServiceConstants = {
