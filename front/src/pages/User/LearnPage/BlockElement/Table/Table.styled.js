91f847d763bd8fd6ba318f88a7cef5cac4c266a5
Vitaliy Prachov <mastersasori007@gmail.com>
2021-07-28 14:53:21 +0300

    feat: add Table mobile view (learn lesson page)

--- front/src/pages/User/LearnPage/BlockElement/Table/Table.styled.js
+++ front/src/pages/User/LearnPage/BlockElement/Table/Table.styled.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
 
+import variables from '@sb-ui/theme/variables';
+
+export const Block = styled.div`
+  padding: 1rem;
+  flex-grow: 1;
+`;
+
+export const BlockHeader = styled.div`
+  color: ${variables['text-table-header']};
+  font-size: 0.75rem;
+`;
+
+export const BlockData = styled.div`
+  margin-top: 1rem;
+`;
+
 export const CustomTableWrapper = styled.div`
   margin-top: 1rem;
   background-color: white;
@@ -14,4 +30,17 @@ export const CustomTable = styled.table`
   td {
     padding: 19px;
   }
+
+  th {
+    font-weight: normal;
+    color: ${variables['text-table-header']};
+    text-align: left;
+    font-size: 0.75rem;
+    padding: 19px;
+  }
+`;
+
+export const CustomMobileTable = styled.div`
+  display: flex;
+  flex-wrap: wrap;
 `;
