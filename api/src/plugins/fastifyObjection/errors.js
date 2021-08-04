b7a6fa612b9ca0e65867ae9e16ea798f03887466
Maksym Herasymov <herasymov.maks@gmail.com>
2021-08-04 12:09:55 +0300

    chore: remove plugin code

--- api/src/plugins/fastifyObjection/errors.js
+++
@@ -1,2 +0,0 @@
-export const ERROR_MISSING_FIELDS = '"connection" and "models" are required';
-export const ERROR_INVALID_ARRAY = '"models" must be a valid array';
--- api/src/plugins/fastifyObjection/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './plugin';
-export { ERROR_MISSING_FIELDS, ERROR_INVALID_ARRAY } from './errors';
