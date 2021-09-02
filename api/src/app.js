0d5288264feec256078018a8978f0fc721d1caf7
Maksym Herasymov <herasymov.maks@gmail.com>
2021-09-02 20:00:45 +0300

    feat: courses service

--- api/src/app.js
+++ api/src/app.js
@@ -16,6 +16,7 @@ import lessonsService from './services/lessons';
 import learnService from './services/learn';
 import lessonsManagementService from './services/lessons-management';
 import coursesManagementService from './services/courses-management';
+import coursesService from './services/courses';
 
 import errorsAndValidation from './validation';
 
@@ -57,6 +58,10 @@ export default (options = {}) => {
 
   app.register(coursesManagementService, {
     prefix: '/api/v1/courses-management',
+  });
+
+  app.register(coursesService, {
+    prefix: '/api/v1/courses',
   });
 
   return app;
---
+++ api/src/services/courses/controllers/getCourse.js
@@ -0,0 +1,46 @@
+const options = {
+  schema: {
+    params: { $ref: 'paramsCourseId#' },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          course: {
+            type: 'object',
+            properties: {
+              id: { type: 'number' },
+              name: { type: 'string' },
+              description: { type: ['string', 'null'] },
+              status: { type: 'string' },
+              author: {
+                type: 'object',
+                properties: {
+                  id: { type: 'number' },
+                  firstName: { type: 'string' },
+                  lastName: { type: 'string' },
+                },
+              },
+            },
+          },
+        },
+      },
+      '4xx': { $ref: '4xx#' },
+      '5xx': { $ref: '5xx#' },
+    },
+  },
+  async onRequest(req) {
+    await this.auth({ req });
+  },
+};
+
+async function handler({ params: { courseId } }) {
+  const {
+    models: { Course },
+  } = this;
+
+  const course = await Course.getCourseWithAuthor({ courseId });
+
+  return { course };
+}
+
+export default { options, handler };
