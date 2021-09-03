f6742cb5560d1d9147cef424930c22e6cf70b1ff
Maksym Herasymov <herasymov.maks@gmail.com>
2021-09-03 16:47:59 +0300

    fix: PR comments

--- api/src/models/Course.js
+++ api/src/models/Course.js
@@ -106,25 +106,22 @@ export default class Course extends BaseModel {
     return this.query(trx).findById(courseId).patch(course).returning('*');
   }
 
-  static getAllMaintainableCourses({
-    userId,
-    offset: start,
-    limit,
-    search,
-    tags,
-  }) {
+  static getAllMaintainableCourses({ userId, offset: start, limit, search }) {
     const end = start + limit - 1;
 
-    const query = this.query()
+    return this.query()
       .skipUndefined()
-      .join('users_roles', 'users_roles.resource_id', '=', 'courses.id')
-      .joinRaw(
-        `left join resource_keywords on courses.id = resource_keywords.resource_id 
-        and resource_keywords.resource_type = '${resources.COURSE.name}'`,
+      .join('users_roles', (builder) =>
+        builder
+          .on('courses.id', '=', 'users_roles.resource_id')
+          .andOn('users_roles.role_id', '=', roles.MAINTAINER.id)
+          .andOn(
+            'users_roles.resource_type',
+            '=',
+            this.knex().raw('?', [resources.COURSE.name]),
+          ),
       )
-      .where('users_roles.role_id', roles.MAINTAINER.id)
-      .andWhere('users_roles.resource_type', resources.COURSE.name)
-      .andWhere('users_roles.user_id', userId)
+      .where('users_roles.user_id', userId)
       .andWhere(
         'courses.name',
         'ilike',
@@ -132,14 +129,6 @@ export default class Course extends BaseModel {
       )
       .groupBy('courses.id')
       .range(start, end)
-      .withGraphFetched('students')
-      .withGraphFetched('keywords');
-
-    if (tags) {
-      return query
-        .whereIn('resource_keywords.keyword_id', tags)
-        .havingRaw('count(resource_keywords.keyword_id) = ?', [tags.length]);
-    }
-    return query;
+      .withGraphFetched('students');
   }
 }
